fb703ecd7e5945f807e2f80bef7611360ccf628b
adamingwersen <adamlinnemann@gmail.com>
2016-08-30 09:49:45 +0200

    useful pkgs

---
+++ 00_Install_Useful_Packages.R
@@ -0,0 +1,19 @@
+# Install all used packages
+
+pkgs <- c('dplyr', 'rvest', 'httr', 'rjsonio', 'caret', 'data.table', 
+          'ggplot2', 'ggthemes', 'plotly', 'mapsDK',
+          'tidyr', 'stringr', 'jsonlite', 'knitr', 'rmarkdown', 'reshape2',
+          'ggmap', 'devtools', 'RCurl', 'Rcpp', 'lubridate',
+          'wordcloud', 'tm', 'RTextTools', 'quanteda', 'MASS',
+          'randomForest', 'lift', 'gbm', 'XML')
+
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+ipak(pkgs)
+
+
